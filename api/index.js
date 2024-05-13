60256583ea24d823534080be9fc8bf3d62a04001
yusuf <yusuffdemirci10@gmail.com>
2024-05-13 16:04:06 +0300

    apiCall and formatUrl functions created

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const API_KEY = "43831679-a74bec2153c4c6ea64436dffb";
+
+const apiUrl = `https://pixabay.com/api/?key=${API_KEY}`;
+
+const formatUrl = (params) => {
+  let url = apiUrl + "&per_page=25&safesearch=true&editors_choice=true";
+  if (!params) return url;
+  let paramKeys = Object.keys(params);
+  paramKeys.map((key) => {
+    let value = key == "q" ? encodeURIComponent(params[key]) : params[key];
+    url += `&${key}=${value}`;
+  });
+  console.log("final url:", url);
+  return url;
+};
+
+export const apiCall = async (params) => {
+  try {
+    const response = await axios.get(formatUrl(params));
+  } catch (err) {
+    console.log("got error", err.message);
+    return { success: false, msg: err.message };
+  }
+};
